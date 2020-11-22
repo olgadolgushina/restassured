035b6683c34ad85cd8b31ad9863307125d35bab1
Olga Dolgushina <dolgushinaolga96@gmail.com>
2020-11-22 17:56:10 +0300

    First commit

---
+++ src/test/java/com/herokuapp/restfulbooker/DeleteBookingTests.java
@@ -0,0 +1,37 @@
+package com.herokuapp.restfulbooker;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+
+public class DeleteBookingTests extends BaseTest {
+
+	@Test
+	public void deleteBookingTests() {
+
+		// Create booking
+		Response responseCreate = createBooking();
+		responseCreate.print();
+
+		// Get bookingId of new booking
+		int bookingid = responseCreate.jsonPath().getInt("bookingid");
+
+		// Delete booking
+		Response responseDelete = RestAssured.given(spec).auth().preemptive().basic("admin", "password123")
+				.delete("/booking/" + bookingid);
+		responseDelete.print();
+
+		// Verifications
+		// Verify response 201
+		Assert.assertEquals(responseDelete.getStatusCode(), 201, "Status code should be 201, but it's not.");
+
+		// more verifications (try to get the booking)
+//				Response responseGet = RestAssured.get("https://restful-booker.herokuapp.com/booking/" + bookingid);
+//				responseGet.print();
+//				
+//				Assert.assertEquals(responseGet.getBody().asString(), "Not Found", "Body should be 'Not Found', but it's not.");
+
+	}
+}
